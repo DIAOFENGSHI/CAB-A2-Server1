05f4e8564eef2ed588efa89c3e7399391bb98c77
root <root@ip-172-31-124-58.ap-southeast-2.compute.internal>
2022-11-05 08:26:09 +0000

    master

--- server/app.js
+++ server/app.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors());
-// app.use(express.static("../432client/build"));
+app.use(express.static("../client/build"));
 
 
 app.get("/init", async (req, res) => {
@@ -92,8 +92,8 @@ app.listen(port, () => {
   console.log("Server listening on port: ", port);
 });
 
-// app.use((req, res) => {
-// res.sendFile(path.join(__dirname, "../432client/build", "index.html"));
-// });
+app.use((req, res) => {
+res.sendFile(path.join(__dirname, "../432client/build", "index.html"));
+});
 
 module.exports = app;
